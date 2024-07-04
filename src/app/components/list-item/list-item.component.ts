ebc79d90c712fca75e6fda64eafe1b3c9b4a4d7f
inga <inga@qupaya.com>
2024-07-04 13:11:22 +0200

    style: add basic styles for list-item

--- src/app/components/list-item/list-item.component.ts
+++ src/app/components/list-item/list-item.component.ts
@@ -1,14 +1,16 @@
 import { Component, input, signal } from '@angular/core';
 import { Artist } from '../../interfaces/artist.interface';
 import { DecimalPipe } from '@angular/common';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-list-item',
   standalone: true,
-  imports: [DecimalPipe],
+  imports: [DecimalPipe, CardComponent],
   templateUrl: './list-item.component.html',
   styleUrl: './list-item.component.css',
 })
 export class ListItemComponent {
   artist = input<Artist>();
+  number = input<number>();
 }
