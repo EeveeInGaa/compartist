196c69c2b5396a99fb7f95da410a72cadee732e9
inga <inga@qupaya.com>
2024-07-06 22:53:40 +0200

    feat: setup translation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,35 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SearchComponent } from '../search/search.component';
 import { Router, RouterLink } from '@angular/router';
 import { SearchResultsComponent } from '../search-results/search-results.component';
 import { SearchService } from '../../services/search.service';
+import {
+  TranslocoDirective,
+  TranslocoPipe,
+  TranslocoService,
+} from '@jsverse/transloco';
+import { Artist } from '../../interfaces/artist.interface';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchComponent, RouterLink, SearchResultsComponent],
+  imports: [
+    SearchComponent,
+    RouterLink,
+    SearchResultsComponent,
+    TranslocoPipe,
+    TranslocoDirective,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   private searchService = inject(SearchService);
   private router = inject(Router);
+  private translocoService = inject(TranslocoService);
+
+  currentLanguage = signal<string>(this.translocoService.getActiveLang());
+  availableLanguages = signal(this.translocoService.getAvailableLangs());
 
   typeSearchTerm(term: string) {
     if (term.trim() === '') {
@@ -24,5 +40,10 @@ export class HeaderComponent {
         queryParams: { term: term },
       });
     }
+  }
+
+  changeLanguage(language: string) {
+    this.currentLanguage.set(language);
+    this.translocoService.setActiveLang(language);
   }
 }
