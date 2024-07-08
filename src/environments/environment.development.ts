034b7643e83194965364b419490525fe739367f0
inga <inga@qupaya.com>
2024-07-08 16:29:43 +0200

    chore: hide api key

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: false,
+  /*
+   * I will send the needed key, since it's not good to publish it.
+   * It would be more elegant with an .env, and I tried it, but it's a lot of effort for this small project.
+   */
   API_KEY: 'ADD_API_KEY_HERE',
 };
